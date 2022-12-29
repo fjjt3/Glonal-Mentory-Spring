e0d4b147f6a769c489c21d6df5d0e55ffe6a82a2
fjjt3 <fjjt3@yahoo.com>
2022-12-29 12:33:18 +0100

    Adding HolaMundo with Thymeleaf and MVC, Created class Persona and List Persona

--- src/main/java/es/fjjt3/Hola/Spring/ControladorInicio.java
+++ src/main/java/es/fjjt3/Hola/Spring/ControladorInicio.java
@@ -1,17 +1,45 @@
 package es.fjjt3.Hola.Spring;
 
+import es.fjjt3.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+@Controller
 @Slf4j
 public class ControladorInicio {
+    @Value("${index.saludo}")
+    private String saludo;
+    @GetMapping("/")
+    public String inicio(Model model){
+        var mensaje = "Mensaje con Thymeleaf";
 
-    @GetMapping
-    public String inicio(){
-        log.info("ejecutando el controlador rest");
-        log.debug("mas detalle del controlador");
-        return "Hallo Welt mit Spring Boot";
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("33333333");
+
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gómez");
+        persona2.setEmail("kgomez@mail.com");
+        persona2.setTelefono("13333333");
+
+        var personas = Arrays.asList(persona, persona2);
+        // var personas = new ArrayList();
+
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje",mensaje);
+        model.addAttribute("saludo", saludo);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
+        return "index";
     }
 }
---
+++ src/main/java/es/fjjt3/domain/Persona.java
@@ -0,0 +1,11 @@
+package es.fjjt3.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
