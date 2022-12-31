f2fb22eba6d119c3f89d9a715eeb98cea1013bfc
fjjt3 <fjjt3@yahoo.com>
2022-12-31 11:53:22 +0100

    Added CRUD operation

--- src/main/java/es/fjjt3/web/ControladorInicio.java
+++ src/main/java/es/fjjt3/web/ControladorInicio.java
@@ -1,27 +1,50 @@
 package es.fjjt3.web;
 
-import es.fjjt3.servicio.PersonaService;
 import lombok.extern.slf4j.Slf4j;
+import es.fjjt3.domain.Persona;
+import es.fjjt3.servicio.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
-
-    @Autowired // brings interface PersonaDao
+    
+    @Autowired
     private PersonaService personaService;
+    
     @GetMapping("/")
     public String inicio(Model model){
         var personas = personaService.listarPersonas();
-
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("personas", personas);
         return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
     }
 }
