f54c21fecd887b8ba94825fd568faf19d32e9d15
fjjt3 <fjjt3@yahoo.com>
2023-01-03 15:57:41 +0100

    Added changes to encript password. Roles and users methods. JPA Security

---
+++ src/main/java/es/fjjt3/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package es.fjjt3.dao;
+
+import es.fjjt3.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long>{
+    Usuario findByUsername(String username);
+}
+
