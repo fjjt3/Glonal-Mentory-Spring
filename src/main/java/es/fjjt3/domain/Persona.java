9ada1c02d56eeb2672af20a9dd739fe6dd8262b3
fjjt3 <fjjt3@yahoo.com>
2023-01-03 11:55:09 +0100

    Added new properties and  WebConfig files, i18 packet

--- src/main/java/es/fjjt3/domain/Persona.java
+++ src/main/java/es/fjjt3/domain/Persona.java
@@ -1,29 +1,31 @@
 package es.fjjt3.domain;
 
-import lombok.Data;
-import lombok.NonNull;
-
+import java.io.Serializable;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import java.io.Serializable;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "persona")
-public class Persona implements Serializable {
+public class Persona implements Serializable{
 
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPersona;
 
     @NotEmpty
     private String nombre;
+
     @NotEmpty
     private String apellido;
+
     @NotEmpty
     @Email
     private String email;
+
     private String telefono;
 }
